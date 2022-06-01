a2b449cb5b8b7be242b34b3c8f36e201cb73c46f
Thaynoss <leray.nicolas7@gmail.com>
2022-06-01 09:53:03 +0200

    m

--- WWW/static/header.php
+++ WWW/static/header.php
@@ -1,11 +1,10 @@
 <header>
-    <div class="container-fluid Header">
+    <div class="container-fluid HeaderBackground">
         <div class="container">
             <div class="row">
                 <!-- col-XX pour gerer les differents type d'appareil avec bootstrap -->
-                <article class="col-xs-12 col-sm-12 col-md-10 col-lg-12 ">
-                    <h1>Simulis by RVL Concept&copy</h1>
-                    <p>La solution pour gérer ses devis !</p>    
+                <article class="col-xs-12 col-sm-12 col-md-10 col-lg-12 HeaderImg ">
+                    <img src="img/logo-rvl-concept-1.jpg">
                 </article>
             </div>
         </div>
